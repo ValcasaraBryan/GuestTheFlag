b5c5dc2a3764f1a548d964138376be27e2ca622d
Valcasara Bryan <31510683+ValcasaraBryan@users.noreply.github.com>
2024-05-16 11:57:52 +0200

    Initial Commit

---
+++ GuestTheFlag/GuestTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuestTheFlagApp.swift
+//  GuestTheFlag
+//
+//  Created by valcasara-bryan on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuestTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
